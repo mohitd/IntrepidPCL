5992a3e22d990cb36128a0b56d7b46e525876649
Mohit Deshpande <mohitd2000@gmail.com>
2015-05-19 13:26:22 -0400

    More robust program since it allows user input for the various filters

--- intrepid_pcl.cpp
+++ intrepid_pcl.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h> 
 #include <ctime>
 
+#include <pcl/PCLPointCloud2.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/io/vtk_io.h>
 #include <pcl/point_types.h>
@@ -30,18 +31,69 @@
 #include <pcl/surface/poisson.h>
 #include <pcl/surface/marching_cubes_rbf.h>
 
+#include <pcl/console/print.h>
+#include <pcl/console/parse.h>
+#include <pcl/console/time.h>
+
 using namespace pcl;
 using namespace pcl::io;
 using namespace pcl::console;
+
+int default_mean_k = 200;
+float default_stdev = 0.1f;
+float default_leaf_size = 0.1f;
+
+void print_help(char **argv)
+{
+	print_error("Syntax is: %s input.pcd output.vtk <options>\n", argv[0]);
+	
+	print_info("\t\t\t\t-mean_k X\t= the number of neighbors to analyze per point (StatisticalOutlierRemoval) (default: ");
+  	print_value("%d", default_mean_k); print_info(")\n");
+  	
+  	print_info("\t\t\t\t-stdev X\t= the threshold for standard deviation (StatisticalOutlierRemoval) (default: ");
+  	print_value("%d", default_stdev); print_info(")\n");
+
+	print_info("\t\t\t\t-leaf_size X\t= the resolution of the grid (VoxelGridFilter) (cubic grid) (default: ");
+  	print_value("%d", default_leaf_size); print_info(")\n");
+}
 
 int main(int argc, char const *argv[])
 {
 	if (argc < 2) 
 	{
-
+		print_help(argv);
+		return -1;
 	}
 
-	ifstream fin("box.txt");
+	// Parse the command line arguments for .pcd files
+	std::vector<int> txt_file_indices;
+	txt_file_indices = parse_file_extension_argument(argc, argv, ".txt");
+	if (txt_file_indices.size () != 1)
+	{
+		print_error("Need one input text file and one output VTK file to continue.\n");
+		return -1;
+	}
+
+	std::vector<int> vtk_file_indices = parse_file_extension_argument(argc, argv, ".vtk");
+	if (vtk_file_indices.size () != 1)
+	{
+		print_error("Need one output VTK file to continue.\n");
+		return -1;
+	}
+
+	int mean_k = default_mean_k;
+	parse_argument(argc, argv, "-mean_k", mean_k);
+	print_info("Setting a mean k of: "); print_value("%f\n", mean_k);
+
+	float stdev = default_stdev;
+	parse_argument(argc, argv, "-stdev", stdev);
+	print_info("Setting a standard deviation of: "); print_value("%f\n", stdev);
+
+	float leaf_size = default_leaf_size;
+	parse_argument(argc, argv, "-leaf_size", leaf_size);
+	print_info("Setting a leaf size of: "); print_value("%f\n", leaf_size);
+
+	ifstream fin(argv[txt_file_indices[0]]);
 
 	int num_data_pts = 0;
     std::string line;
@@ -78,20 +130,22 @@ int main(int argc, char const *argv[])
 	PointCloud<PointXYZ>::Ptr cloud_filtered2 (new PointCloud<PointXYZ>());
 	
 	// create the filtering object
-	int k = 200;
-	float stdev = 0.1f;
 	StatisticalOutlierRemoval<PointXYZ> sor;
   	sor.setInputCloud (cloud2);
-  	sor.setMeanK(k); // analyzes 1000 neighbors
+  	sor.setMeanK(mean_k); // analyzes 1000 neighbors
   	sor.setStddevMulThresh(stdev); // remove all points who have a distance larger than 1 standard deviation of the mean distance
+
+  	TicToc tt1;
+  	tt1.tic();
+  	print_highlight("Computing StatisticalOutlierRemoval ");
   	sor.filter(*cloud_filtered); // query point will be marked as outliers and removed
+  	print_info("[done, "); print_value("%g", tt1.toc ()); print_info(" ms]\n");
   	
   	std::cout << "Stats filtered cloud is now " << cloud_filtered->points.size() << std::endl;
 
-	float size = 0.3f;
   	VoxelGrid<PointXYZ> voxelFilter;
   	voxelFilter.setInputCloud(cloud_filtered);
-  	voxelFilter.setLeafSize(size, size, size);	// create voxels of side length 1cm (0.01m)
+  	voxelFilter.setLeafSize(leaf_size, leaf_size, leaf_size);	// create voxels of side length 1cm (0.01m)
   	voxelFilter.filter(*cloud_filtered2);
 	
 	std::cout << "Voxel filtered cloud is now " << cloud_filtered2->points.size() << std::endl;
@@ -146,8 +200,8 @@ int main(int argc, char const *argv[])
   	gp3.reconstruct (triangles);
 	
 
-	saveVTKFile ("out/mesh.vtk", triangles);
+	saveVTKFile ("out/" + argv[vtk_file_indices[0]], triangles);
 
-	std::cout << "Saved final mesh to out/mesh.vtk" << std::endl;
+	std::cout << "Saved final mesh to out/" << argv[vtk_file_indices[0]] << std::endl;
 	return 0;
 }
